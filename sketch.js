532a0c772960af4662832c381ba265cf2464d670
SMaribbay <78175106+SMaribbay@users.noreply.github.com>
2021-05-04 15:17:11 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let paddlePlayer;
 let paddleAi;
- 
+let ball;
+let scorePlayer;
+let scoreAi;
+
 function setup() {
   createCanvas(634, 350);
   paddlePlayer = new Paddle(26);
   paddleAi = new Paddle (width-48);
+  ball = new Ball();
 }
  
 function draw() {
@@ -12,13 +16,44 @@ function draw() {
   background(200);
   paddlePlayer.display();
   paddleAi.display();
-  if (paddlePlayer.isUp) {
+  
+  paddlePlayer.update();
+  paddleAi.update();
+  
+    if (paddlePlayer.isUp) {
     paddlePlayer.up();
   } else if (paddlePlayer.isDown) {
     paddlePlayer.down();
+    
   }
-     
-  //paddlePlayer.up();
+  processAi();
+  
+  ball.update(scorePlayer,scoreAi);
+  ball.display();
+  
+  //ball.hitPlayer(paddlePlayer);
+  //ball.hitAi(paddleAi);
+  
+  stroke(0);
+  line(width/2, 0, width/2, height);
+  
+  //scorePlayer.display();
+  //scoreAi.display();
+
+     ball.update(); // call the update function within ball
+    ball.display();
+  
+}
+function processAi(){
+  let middlePaddle = paddleAi.y +paddleAi.height/2;
+  if (middlePaddle >ball.y){
+    paddleAi.isUp=true;
+    paddleAi.isDown=false;
+  }else{
+    paddleAi.isDown=true;
+    paddleAi.isUp = false;
+    
+  }
 }
 function keyPressed(){
   if (keyCode == UP_ARROW) {
